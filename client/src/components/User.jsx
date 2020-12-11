0f56734905b6691abdc1edbb280c53887ba1047d
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-12-11 15:30:43 -0500

    further styling on Login

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -4,15 +4,15 @@ import Register from './Register'
 
 import './User.css'
 
-export default function User({ formData, login, register, setFormData}) {
+export default function User({ formData, login, register, setFormData }) {
 
   const [tab, updateTab] = useState(true)
 
   return (
     <div className="register-login-main">
       <div className="user-tabs">
-        <h1 onClick={() => updateTab(true)}>Register</h1>
-        <h1 onClick={() => updateTab(false)}>Login</h1>
+        <h1 id={tab ? 'small' : null} onClick={() => updateTab(true)}>Register</h1>
+        <h1 id={!tab ? 'small' : null} onClick={() => updateTab(false)}>Login</h1>
       </div>
       { tab ?
         <Register
