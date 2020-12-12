5124517f0d12e58c489c8b2066c8b903faadcd3f
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-12-12 14:07:16 -0500

    added production api baseURL

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const expressAPI = process.env.NODE_ENV === "development" ? 'http://localhost:3000' : null
+const expressAPI = process.env.NODE_ENV === "development" ? 'http://localhost:3000' : 'https://will-do-express.herokuapp.com/'
 
 // Creating a new instance of axios that will set the baseURL for requests/responses to be PORT 3000
 export const api = axios.create({
