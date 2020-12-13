631bc69493d278064628b6ab0074f48936912ed2
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-12-13 15:02:02 -0500

    reorganized completed item component and styles

--- client/src/components/CompletedList.jsx
+++ client/src/components/CompletedList.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 
 import DeletionModal from './DeletionModal'
 import CompletionModal from './CompletionModal'
-import HoverPopOver from './HoverPopOver'
-
-import deleteIcon from '../images/delete-wht.png'
+import CompletedItems from './CompletedItems'
 
 import './CompletedList.css'
 
@@ -14,53 +11,26 @@ export default function CompletedList(props) {
   const [formData, setFormData] = useState()
   const [deletionItemID, setDeletionItemID] = useState(null)
   const [completionItemID, setCompletionItemID] = useState(null)
-  const [popOver, togglePopOver] = useState(false)
-
-  const completedList = props.completed.map((item) => {
-    return (
-      <div key={item.id} className="completed-item">
-        <li>{item.fields.name}</li>
-        <section className="completed-options">
-          <Link to={`/items/${item.id}`} className="completed-link">Details</Link>
-          <img
-            src={deleteIcon} alt="delete"
-            className="completed-delete"
-            onClick={() => setDeletionItemID(item.id)}
-          />
-          <input
-            id="complete"
-            type="checkbox"
-            checked={completionItemID === item.id ? false : true}
-            value={item.id}
-            onMouseOver={(e) => togglePopOver(e.target.value)}
-            onMouseLeave={() => togglePopOver(false)}
-            onClick={(e) => {
-              if (!e.target.checked) {
-                const randoIdx = Math.floor(Math.random() * props.gifs.length)
-                props.setGif(props.gifs[randoIdx])
-                setCompletionItemID(item.id)
-                setFormData({
-                  name: item.fields.name,
-                  description: item.fields.description,
-                  priority: 0,
-                  email: [props.currentUser.id],
-                  complete: false
-                })
-              }
-            }}
-          />
-          {popOver === item.id && <HoverPopOver />}
-        </section>
-      </div>
-    )
-  })
 
   return (
     <div className="completed-main">
       <div className="completed-back">
         <h1 className="completed-title">Completed LIST</h1>
         <ol>
-          {completedList}
+          {props.completed.map((item) => {
+            return (
+              <CompletedItems
+                item={item}
+                setFormData={setFormData}
+                setDeletionItemID={setDeletionItemID}
+                completionItemID={completionItemID}
+                setCompletionItemID={setCompletionItemID}
+                currentUser={props.currentUser}
+                gifs={props.gifs}
+                setGif={props.setGif}
+              />
+            )
+          })}
         </ol>
       </div>
 
